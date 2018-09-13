b71b3351ee6be682f437392c2f9a8791954339bd
Bestush77 <bestun95@yahoo.com>
2018-09-13 10:18:29 -0400

    added toShoshowOnDisplay and add function

--- indexJS.js
+++ indexJS.js
@@ -1,3 +1,22 @@
-let displayContent = document.getElementById("displayContent");
+let display = document.getElementById("displayContent");
+let displayExpression = [1, " + ", 2];
 
-displayContent.innerHTML = 'efwffdw';
+display.innerHTML = showOnDisplay(displayExpression);
+
+
+//add
+function add(myArray) {
+    return myArray.reduce((accumulator, currrentValue) => accumulator + currrentValue)
+}
+
+
+//returns array as string for display
+function showOnDisplay(array) {
+    let m = "";
+    array.forEach((el) => {
+        m += el
+    })
+    return m;
+}
+
+console.log(displayContent);
